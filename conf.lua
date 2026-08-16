43d48de267019cc5a2de17b77f3bf62842950ed6
i <isomorphisms@sdf.org>
2026-08-16 00:10:54 -0400

    Add LÖVE2D configuration

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.identity = "game"
+    t.console = false
+
+    t.window.title = "Game"
+    t.window.width = 960
+    t.window.height = 540
+    t.window.resizable = true
+    t.window.vsync = 1
+end
